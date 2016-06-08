481005f78847706b6a196b64f7e0f6fa4b2e82f2
Hugo Santos Piauilino <hugos94@gmail.com>
2016-06-08 16:49:33 -0300

    Adicionados os cincos metodos que serao utilizados e as discretizacoes. Serao 3 simulacoes para cada metodo.

--- main.c
+++ main.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+int main(int argc, const char* argv[]){
+
+    int tam[9] = {100, 500, 1000, 5000, 30000, 80000, 100000, 150000, 200000}; //Vetor com os tamanhos de vetores para testes
+    int i, position = 0, qntd = 0, partition = 0;
+
+    printf("Tamanhos de Vetores:\n1 - 100\n2 - 500\n3 - 1000\n4 - 5000\n5 - 30000\n6 - 80000\n7 - 100000\n8 - 150000\n9 - 200000\n\n");
+
+    printf("Informe o tamanho do vetor: ");
+    scanf("%d", &position);
+
+    qntd = tam[position-1]; //Armazena o tamanho do vetor escolhido
+
+    int vet[qntd]; //Cria o vetor com o tamanho escolhido
+
+    srand(time(NULL)); //Reinicia a semente aleatória
+
+    for(i = 0; i < qntd;i++){ //Percorre todo o vetor adicionado os valores aleatórios
+        vet[i] = (rand()%qntd); //Armazena no vetor o valor aleatório gerado
+    }
+
+    //const char *metodos[] = {"1 - Hoare","2 - Lomuto", "3 - One-sided", "4 - Gries", "5 - Sedgewick"};
+
+    printf("Métodos de Partição:\n1 - Hoare\n2 - Lomuto\n3 - One-sided\n4 - Gries\n5 - Sedgewick\n\n");
+
+    printf("Informe o método de partição a ser utilizado: ")
+    scanf("%d", &partition);
+
+    quickSort(vet,0,qntd-1); //Chama a função QuickSort
+
+    for(i = 0; i < qntd; i++){
+      printf("%d ", vet[i]);
+    }
+    printf("\n");
+
+
+    return 0;
+}
 
 void quickSort(int vet[], int esq, int dir){
   int i, j, pivo, aux;
   i = esq;
   j = dir;
   pivo = vet[(esq+dir)/2];
-  printf("Pivo: %d\n", pivo);
 
   while(i <= j){
     while(vet[i] < pivo && i < dir){
@@ -16,7 +54,6 @@ void quickSort(int vet[], int esq, int dir){
         j--;
     }
     if(i <= j){
-        printf("Trocas: %d %d\n", vet[i], vet[j]);
         aux = vet[i];
         vet[i] = vet[j];
         vet[j] = aux;
@@ -30,36 +67,9 @@ void quickSort(int vet[], int esq, int dir){
     printf("\n");
   }
   if(j > esq){
-    printf("Ramo esquerdo:\n");
     quickSort(vet, esq, j);
   }
   if(i < dir){
-      printf("Ramo direito:\n");
     quickSort(vet,i, dir);
   }
-}
-
-int main(int argc, const char* argv[]){
-
-    int tam = 10, i;
-    int vet[10] = {8,1,7,2,3,4,5,9,0,6};
-    /*
-    printf("Informe o tamanho do vetor: ");
-    scanf("%d", &tam);
-
-    int vet[tam], i;
-
-    for(i = 0; i < tam; i++){
-      scanf("%d", &vet[i]);
-    }
-    */
-
-    quickSort(vet,0,tam-1);
-
-    for(i = 0; i < tam; i++){
-      printf("%d ", vet[i]);
-    }
-    printf("\n");
-
-    return 0;
 }
