a774e750187e375460b84b69486f4a0c9f4c172c
Hugo Santos Piauilino <hugos94@gmail.com>
2016-06-09 18:18:55 -0300

    Metodos de lomuto e hoare implementados e prontos para serem testados.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
 int main(int argc, const char* argv[]){
@@ -21,91 +21,72 @@ int main(int argc, const char* argv[]){
 
     for(i = 0; i < qntd;i++){ //Percorre todo o vetor adicionado os valores aleatórios
         vet[i] = (rand()%qntd); //Armazena no vetor o valor aleatório gerado
+        //printf("%d ", vet[i]);
     }
+    //printf("\n");
 
-    //const char *metodos[] = {"1 - Hoare","2 - Lomuto", "3 - One-sided", "4 - Gries", "5 - Sedgewick"};
 
-    printf("Métodos de Partição:\n1 - Hoare\n2 - Lomuto\n3 - One-sided\n4 - Gries\n5 - Sedgewick\n\n");
-
-    printf("Informe o método de partição a ser utilizado: ");
-    scanf("%d", &partition);
-
-    quickSort(vet,0,qntd-1); //Chama a função QuickSort
+    quickSort(&vet,0,qntd); //Chama a função QuickSort
 
     for(i = 0; i < qntd; i++){
       printf("%d ", vet[i]);
     }
     printf("\n");
 
-
     return 0;
 }
 
-void quickSort(int vet[], int esq, int dir){
-  int i, j, pivo, aux;
-  i = esq;
-  j = dir;
-  pivo = vet[(esq+dir)/2];
-
-  while(i <= j){
-    while(vet[i] < pivo && i < dir){
-        i++;
-    }
-    while(vet[j] > pivo && j > esq){
-        j--;
-    }
-    if(i <= j){
-        aux = vet[i];
-        vet[i] = vet[j];
-        vet[j] = aux;
-        i++;
-        j--;
-    }
-    int cont;
-    for(cont = 0; cont < 10; cont++){
-        printf("%d ", vet[cont]);
-    }
-    printf("\n");
-  }
-  if(j > esq){
-    quickSort(vet, esq, j);
-  }
-  if(i < dir){
-    quickSort(vet,i, dir);
-  }
+void swap(int *a, int i, int j)
+{
+    int t = a[i];
+    a[i] = a[j];
+    a[j] = t;
 }
 
-int hoare_partition(int vet[], int esq, int dir){
-    int x = vet[esq];
-    int i = esq - 1;
-    int j = dir + 1;
+void quickSort(int *vet, int esq, int dir){
+    int q = 0;
+    /*
+    if(esq < dir){
+        q = lomuto_partition(vet, esq, dir);
+        quickSort(vet, esq, q-1);
+        quickSort(vet, q+1, dir);
+    }
+    */
+    ///*
+    if(esq < dir){
+        q = hoare_partition(vet, esq, dir);
+        quickSort(vet, esq, q);
+        quickSort(vet, q+1, dir);
+    }
+    //*/
+}
+
+int hoare_partition(int *vet, int esq, int dir){
+    int pivot = vet[esq], i = esq - 1, j = dir + 1;
     while(true){
-        while(vet[j] <= x){
-            j--;
-        }
-        while(vet[i] >= x){
+        do{
             i++;
+        }while(vet[i] < pivot);
+        do{
+            j--;
+        }while(vet[j] > pivot);
+        if(i >= j){
+            return j;
         }
-        if(i < j){
-            swap(vet[i], vet[j]);
-        }else{
-        return j;
-        }
+        swap(vet, i, j);
     }
 }
 
-int lomuto_partition(int vet[], int esq, int dir){
-    int x = vet[dir];
-    int i = esq - 1;
-    int j;
+int lomuto_partition(int *vet, int esq, int dir){
+    int x = vet[dir],i = esq - 1,j = esq;
     for(j = esq; j <= dir-1; j++){
         if(vet[j] <= x){
             i++;
-            swap(vet[i], vet[j]);
+            swap(vet, i, j);
         }
     }
-    swap(vet[i+1], vet[dir]);
+    swap(vet, i+1, dir);
     return (i+1);
 }
 
-void swap(int a, int b){int aux = a; a = b; b = aux;}
+
