b37ab0d41871dfa4b369b8d513af99e893c66db6
Hugo Santos Piauilino <hugos94@gmail.com>
2016-06-10 17:16:28 -0300

    Simulações realizadas. Arquivo com os resultados adicionado.

--- main.c
+++ main.c
@@ -8,12 +8,12 @@ int main(int argc, const char* argv[]){
     int tam[9] = {100, 500, 1000, 5000, 30000, 80000, 100000, 150000, 200000}; //Vetor com os tamanhos de vetores para testes
     int i, position = 0, qntd = 0, partition = 0;
 
-    printf("Tamanhos de Vetores:\n1 - 100\n2 - 500\n3 - 1000\n4 - 5000\n5 - 30000\n6 - 80000\n7 - 100000\n8 - 150000\n9 - 200000\n\n");
+    //printf("Tamanhos de Vetores:\n1 - 100\n2 - 500\n3 - 1000\n4 - 5000\n5 - 30000\n6 - 80000\n7 - 100000\n8 - 150000\n9 - 200000\n\n");
 
-    printf("Informe o tamanho do vetor: ");
-    scanf("%d", &position);
+    //printf("Informe o tamanho do vetor: ");
+    //scanf("%d", &position);
 
-    qntd = tam[position-1]; //Armazena o tamanho do vetor escolhido
+    qntd = tam[8]; //Armazena o tamanho do vetor escolhido
 
     int vet[qntd]; //Cria o vetor com o tamanho escolhido
 
@@ -28,10 +28,10 @@ int main(int argc, const char* argv[]){
 
     quickSort(&vet,0,qntd); //Chama a função QuickSort
 
-    for(i = 0; i < qntd; i++){
-      printf("%d ", vet[i]);
-    }
-    printf("\n");
+    //for(i = 0; i < qntd; i++){
+      //printf("%d ", vet[i]);
+    //}
+    //printf("\n");
 
     return 0;
 }
@@ -44,21 +44,21 @@ void swap(int *a, int i, int j)
 }
 
 void quickSort(int *vet, int esq, int dir){
-    int q = 0;
-    /*
+    int q;
+    ///*
     if(esq < dir){
         q = lomuto_partition(vet, esq, dir);
         quickSort(vet, esq, q-1);
         quickSort(vet, q+1, dir);
     }
-    */
-    ///*
+    //*/
+    /*
     if(esq < dir){
         q = hoare_partition(vet, esq, dir);
         quickSort(vet, esq, q);
         quickSort(vet, q+1, dir);
     }
-    //*/
+    */
 }
 
 int hoare_partition(int *vet, int esq, int dir){
